96a17b2cd0e6d3e888408bcf5a1726ebe209acb8
theodumez31 <152598830+theodumez31@users.noreply.github.com>
2024-06-11 16:49:12 +0200

    Update RPC.py

--- RPC.py
+++ RPC.py
@@ -19,3 +19,22 @@ class RequestHandler(SimpleXMLRPCRequestHandler):
 # Créer un serveur XML-RPC
 server = SimpleXMLRPCServer(("localhost", 8000), requestHandler=RequestHandler, allow_none=True)
 server.register_introspection_functions()
+
+def get_siren_data(siren):
+    """
+    Retourne les données EgaPro pour un numéro SIREN donné.
+    Un message d'erreur est retourné si le SIREN n'est pas trouvé.
+
+    :param siren: Numéro SIREN en tant que chaîne de caractères
+    :return: Les données correspondantes ou un message d'erreur
+    """
+    data = egapro_data.get(siren)
+    if data is None:
+        return {"erreur": "SIREN non trouvé"}
+    return data
+
+server.register_function(get_siren_data, 'get_siren_data')
+
+# Exécuter le serveur XML-RPC
+print("Le serveur RPC est en cours d'exécution sur http://localhost:8000/RPC2")
+server.serve_forever()
